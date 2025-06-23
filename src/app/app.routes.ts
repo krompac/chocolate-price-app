5fddd2169568a803a5b1f8292d5d8c9e3e2d9b61
krompac <tomijehrvat@gmail.com>
2025-06-23 22:49:54 +0200

    Chocolate details page with resolver.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
-import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
+import { ChocolatesService } from './core/services/chocolates.service';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
-      import(
-        './features/chocolates-overview/chocolates-overview.component'
-      ).then((component) => component.ChocolatesOverviewComponent),
+      import('./features/chocolates-overview/chocolates-overview.component').then(
+        (component) => component.ChocolatesOverviewComponent
+      )
   },
   {
     path: ':id',
@@ -14,5 +16,13 @@ export const routes: Routes = [
       import('./features/chocolate-details/chocolate-details.component').then(
         (component) => component.ChocolateDetailsComponent
       ),
-  },
+    resolve: {
+      chocolate: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+        const chocolatesService = inject(ChocolatesService);
+        const id = route.paramMap.get('id')!;
+
+        return chocolatesService.getChocolateById(id);
+      }
+    }
+  }
 ];
--- src/app/features/chocolate-details/chocolate-details.component.ts
+++ src/app/features/chocolate-details/chocolate-details.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FullChocolateModel } from '../../core/model/chocolate.model';
 
 @Component({
   selector: 'app-chocolate-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './chocolate-details.component.html',
   styleUrl: './chocolate-details.component.less'
 })
-export class ChocolateDetailsComponent {}
+export class ChocolateDetailsComponent {
+  /* Dependency injections */
+  private readonly activatedRoute = inject(ActivatedRoute);
+
+  readonly chocolate = this.activatedRoute.snapshot.data['chocolate'] as FullChocolateModel;
+}
